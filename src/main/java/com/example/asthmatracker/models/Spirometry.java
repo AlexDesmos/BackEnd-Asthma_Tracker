aaa976df93291b432d7f56752c7df7827031ea91
Maksim <mku.dmi@gmail.com>
2025-05-05 21:24:20 +0300

    some changes

--- src/main/java/com/example/asthmatracker/models/Spirometry.java
+++ src/main/java/com/example/asthmatracker/models/Spirometry.java
@@ -11,6 +11,6 @@ import java.time.LocalDateTime;
 public class Spirometry {
     private Integer id;
     private Integer patient_id;
-    private BigDecimal result;
+    private Integer result;
     private LocalDateTime date_time;
 }
